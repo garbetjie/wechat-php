c470836a2199d1b769991f798619c060cfdc239f
Geoff Garbers <geoff@garbers.co.za>
2016-03-22 09:44:17 +0200

    updated error handling for menus

--- src/Menu/Service.php
+++ src/Menu/Service.php
@@ -2,6 +2,7 @@
 
 namespace Garbetjie\WeChatClient\Menu;
 
+use Garbetjie\WeChat\Client\Exception\ApiErrorException;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Psr7\Request;
 use InvalidArgumentException;
@@ -19,7 +20,7 @@ class Service
      *
      * @param Client $client
      */
-    public function __construct ( Client $client )
+    public function __construct (Client $client)
     {
         $this->client = $client;
     }
@@ -29,52 +30,47 @@ class Service
      *
      * @param Menu $menu
      *
-     * @throws Exception
+     * @throws GuzzleException
+     * @throws ApiErrorException
      */
-    public function create ( Menu $menu )
+    public function create (Menu $menu)
     {
-        try {
-            $json = json_encode( $this->reduceMenu( $menu ) );
-            $request = new Request( 'POST', "https://api.weixin.qq.com/cgi-bin/menu/create", [ ], $json );
-            $this->client->send( $request );
-        } catch ( GuzzleException $e ) {
-            throw new Exception( "Cannot create menu. HTTP error occurred.", null, $e );
-        }
+        $json = json_encode($this->reduceMenu($menu));
+        $request = new Request('POST', "https://api.weixin.qq.com/cgi-bin/menu/create", [], $json);
+        $this->client->send($request);
     }
 
     /**
      * Deletes the current menu in the OA.
      *
-     * @throws Exception
+     * @throws GuzzleException
+     * @throws ApiErrorException
      */
     public function delete ()
     {
-        try {
-            $request = new Request( 'GET', "https://api.weixin.qq.com/cgi-bin/menu/delete" );
-            $this->client->send( $request );
-        } catch ( GuzzleException $e ) {
-            throw new Exception( "Cannot delete menu. HTTP error occurred.", null, $e );
-        }
+        $request = new Request('GET', "https://api.weixin.qq.com/cgi-bin/menu/delete");
+        $this->client->send($request);
     }
 
     /**
-     * Retrieve the current menu for the OA, and return it as an instance of `WeChat\Menu\Menu`.
+     * Retrieve the current menu for the OA, and return it as an instance of `Garbetjie\WeChatClient\Menu\Menu`.
      *
      * @return Menu
+     * 
+     * @throws GuzzleException
+     * @throws ApiErrorException
      * @throws Exception
      */
     public function fetch ()
     {
         try {
-            $request = new Request( "GET", "https://api.weixin.qq.com/cgi-bin/menu/get" );
-            $response = $this->client->send( $request );
-            $json = json_decode( (string) $response->getBody(), true );
+            $request = new Request("GET", "https://api.weixin.qq.com/cgi-bin/menu/get");
+            $response = $this->client->send($request);
+            $json = json_decode((string)$response->getBody(), true);
 
-            return $this->inflateMenu( $json );
-        } catch ( GuzzleException $e ) {
-            throw new Exception( "Cannot fetch menu. HTTP error occurred.", null, $e );
-        } catch ( InvalidArgumentException $e ) {
-            throw new Exception( "Cannot build fetched menu. Invalid JSON response encountered.", null, $e );
+            return $this->inflateMenu($json);
+        } catch (InvalidArgumentException $e) {
+            throw new Exception("cannot build fetched menu. invalid JSON.", null, $e);
         }
     }
 
@@ -86,10 +82,10 @@ class Service
      *
      * @return bool
      */
-    public function validates ( Menu $menu )
+    public function validates (Menu $menu)
     {
-        foreach ( $menu->items() as $item ) {
-            if ( ! $this->validateItem( $item ) ) {
+        foreach ($menu->items() as $item) {
+            if (! $this->validateItem($item)) {
                 return false;
             }
         }
@@ -104,35 +100,35 @@ class Service
      *
      * @return bool
      */
-    protected function validateItem ( Item $item )
+    protected function validateItem (Item $item)
     {
         // Item has children.
-        if ( count( $item->children() ) > 0 ) {
-            foreach ( $item->children() as $childItem ) {
-                if ( ! $this->validateItem( $childItem ) ) {
+        if (count($item->children()) > 0) {
+            foreach ($item->children() as $childItem) {
+                if (! $this->validateItem($childItem)) {
                     return false;
                 }
             }
 
             // If there are children, the title cannot be longer than 16 characters.
-            if ( mb_strlen( $item->title() ) > 16 ) {
+            if (strlen($item->title()) > 16) {
                 return false;
             }
+            
+            return true;
         }
 
         // Title cannot be longer than 40 characters.
-        if ( $item->title() > 40 ) {
+        if (strlen($item->title()) > 40) {
             return false;
         }
 
         // URLs cannot be longer than 256 characters.
-        if ( $item->type() == Item::URL && mb_strlen( $item->key() ) > 256 ) {
+        if ($item->type() == Item::URL && strlen($item->key()) > 256) {
             return false;
-        } // Any other key cannot be longer than 128 characters.
-        else if ( mb_strlen( $item->key() ) > 128 ) {
+        } elseif (strlen($item->key()) > 128) {
             return false;
-        } // Keys cannot be empty.
-        else if ( mb_strlen( $item->key() ) < 1 ) {
+        } elseif (strlen($item->key()) < 1) {
             return false;
         }
 
@@ -146,12 +142,12 @@ class Service
      *
      * @return array
      */
-    protected function reduceMenu ( Menu $menu )
+    protected function reduceMenu (Menu $menu)
     {
-        $reduced = [ 'button' => [ ] ];
+        $reduced = ['button' => []];
 
-        foreach ( $menu->items() as $menuItem ) {
-            $reduced[ 'button' ][] = $this->reduceItem( $menuItem );
+        foreach ($menu->items() as $menuItem) {
+            $reduced['button'][] = $this->reduceItem($menuItem);
         }
 
         return $reduced;
@@ -164,20 +160,20 @@ class Service
      *
      * @return array
      */
-    protected function reduceItem ( Item $item )
+    protected function reduceItem (Item $item)
     {
-        $reduced = [ 'name' => $item->title() ];
+        $reduced = ['name' => $item->title()];
 
         // Has children.
-        if ( count( $item->children() ) > 0 ) {
-            $reduced[ 'sub_button' ] = [ ];
-            foreach ( $item->children() as $childItem ) {
-                $reduced[ 'sub_button' ][] = $this->reduceItem( $childItem );
+        if (count($item->children()) > 0) {
+            $reduced['sub_button'] = [];
+            foreach ($item->children() as $childItem) {
+                $reduced['sub_button'][] = $this->reduceItem($childItem);
             }
         } // No children.
         else {
-            $reduced[ 'type' ] = $item->type();
-            $reduced[ $item->type() === Item::URL ? 'url' : 'key' ] = $item->key();
+            $reduced['type'] = $item->type();
+            $reduced[$item->type() === Item::URL ? 'url' : 'key'] = $item->key();
         }
 
         return $reduced;
@@ -189,44 +185,45 @@ class Service
      * @param array $deflated
      *
      * @return Menu
+     * 
      * @throws InvalidArgumentException
      */
-    protected function inflateMenu ( array $deflated )
+    protected function inflateMenu ($deflated)
     {
-        if ( ! isset( $deflated[ 'menu' ][ 'button' ] ) ) {
-            throw new InvalidArgumentException( 'Unexpected menu JSON structure: `menu.button` not found' );
+        if (! isset($deflated['menu']['button'])) {
+            throw new InvalidArgumentException('Unexpected menu JSON structure: `menu.button` not found');
         }
 
         $menu = new Menu();
 
-        foreach ( $deflated[ 'menu' ][ 'button' ] as $item ) {
-            $menu->add( $this->inflateItem( $item ) );
+        foreach ($deflated['menu']['button'] as $item) {
+            $menu->add($this->inflateItem($item));
         }
 
         return $menu;
     }
 
     /**
-     * Inflate the given array representation of an item, and return it as an instance of `WeChat\Menu\Item`.
+     * Inflate the given array representation of an item, and return it as an instance of `Garbetjie\WeChatClient\Menu\Item`.
      *
      * @param array $item
      *
      * @return Item
      */
-    protected function inflateItem ( array $item )
+    protected function inflateItem (array $item)
     {
-        if ( isset( $item[ 'sub_button' ] ) & count( $item[ 'sub_button' ] ) > 0 ) {
-            $object = new Item( $item[ 'name' ], Item::KEYWORD );
-            foreach ( $item[ 'sub_button' ] as $subItem ) {
-                $object->add( $this->inflateItem( $subItem ) );
+        if (isset($item['sub_button']) & count($item['sub_button']) > 0) {
+            $object = new Item($item['name'], Item::KEYWORD);
+            foreach ($item['sub_button'] as $subItem) {
+                $object->add($this->inflateItem($subItem));
             }
         } else {
-            if ( isset( $item[ 'url' ] ) ) {
-                $object = new Item( $item[ 'name' ], $item[ 'type' ], $item[ 'url' ] );
-            } else if ( isset( $item[ 'key' ] ) ) {
-                $object = new Item( $item[ 'name' ], $item[ 'type' ], $item[ 'key' ] );
+            if (isset($item['url'])) {
+                $object = new Item($item['name'], $item['type'], $item['url']);
+            } elseif (isset($item['key'])) {
+                $object = new Item($item['name'], $item['type'], $item['key']);
             } else {
-                $object = new Item( $item[ 'name' ], $item[ 'type' ] );
+                $object = new Item($item['name'], $item['type']);
             }
         }
 
