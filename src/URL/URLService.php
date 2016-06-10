7dff9034999dd7de74654a5f59d2263f0ab2d02f
Geoff Garbers <geoff@garbers.co.za>
2016-06-10 22:44:51 +0200

    Removed separate bulk service.

--- src/URL/URLService.php
+++ src/URL/URLService.php
@@ -4,6 +4,8 @@ namespace Garbetjie\WeChatClient\URL;
 
 use Garbetjie\WeChatClient\Service;
 use Garbetjie\WeChatClient\URL\Exception\URLException;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Pool;
 use GuzzleHttp\Psr7\Request;
 use GuzzleHttp\RequestOptions;
 use Psr\Http\Message\RequestInterface;
@@ -13,30 +15,146 @@ use Psr\Http\Message\UriInterface;
 class URLService extends Service
 {
     /**
+     * Shortens all the given URLs.
+     *
+     * If a callback is given, it needs to have the following signature:
+     * `function (RequestException $error = null, $short = '', $long = '')`
+     *
+     * If an error occurs attempting to shorten a URL, the $error parameter will be populated with an instance of
+     * `GuzzleHttp\Exception\RequestException`.
+     *
+     * If no callback is given, an array containing a long => short URL mapping is returned.
+     *
+     * @param array    $urls
+     * @param callable $callback
+     *
+     * @return array
+     */
+    public function shortenAll (array $urls, callable $callback = null)
+    {
+        // Create output.
+        $urls = array_values($urls);
+        $shortened = [];
+
+        // Create request builder.
+        $requests = function ($urls) {
+            foreach ($urls as $url) {
+                yield new Request(
+                    'POST',
+                    'https://api.weixin.qq.com/cgi-bin/shorturl',
+                    [],
+                    json_encode([
+                        'action'   => 'long2short',
+                        'long_url' => $url,
+                    ])
+                );
+            }
+        };
+
+        if (! is_callable($callback)) {
+            $shortened = array_combine($urls, array_pad([], count($urls), null));
+            $callback = function ($error, $short, $long) use (&$shortened) {
+                if ($error === null) {
+                    $shortened[$long] = $short;
+                }
+            };
+        }
+
+        // Send off requests.
+        (new Pool(
+            $this->client,
+            $requests($urls),
+            [
+                'fulfilled' => function (ResponseInterface $response, $index) use ($urls, $callback) {
+                    $json = json_decode((string)$response->getBody());
+                    $long = $urls[$index];
+                    $short = isset($json->short_url) ? $json->short_url : null;
+
+                    call_user_func($callback, null, $short, $long);
+                },
+                'rejected'  => function (RequestException $reason) use (&$failed, $callback) {
+                    call_user_func($callback, $reason, null, null);
+                },
+            ]
+        ))->promise()->wait();
+
+        // Return the shortened URLs.
+        return $shortened;
+    }
+    
+    /**
      * Returns a shorter representation of the given URL. Similar to URL shortening services like bit.ly, etc.
      *
      * @param string $url
      *
-     * @return string
-     *
-     * @throws URLException
+     * @return string|null
      */
     public function shorten ($url)
     {
-        $json = json_encode([
-            'action'   => 'long2short',
-            'long_url' => $url,
-        ]);
+        $shortened = null;
+        
+        $this->shortenAll([$url], function ($error, $short, $long) use (&$shortened) {
+            $shortened = $short;
+        });
+        
+        return $shortened;
+    }
 
-        $request = new Request('POST', 'https://api.weixin.qq.com/cgi-bin/shorturl', [], $json);
-        $response = $this->client->send($request);
-        $json = json_decode((string)$response->getBody());
+    /**
+     * Expands the given URLs into their long version. This is done dirtily, by issuing a HEAD request to the given
+     * short URL, and using the contents of the `Location` header.
+     *
+     * If a callback is given, it needs to have the following signature:
+     * `function ($long = '', $short = '')`
+     *
+     * If a callback isn't specified, an array containing a mapping of short => long URLs is returned.
+     *
+     * @param array         $urls
+     * @param callable|null $callback
+     *
+     * @return array
+     */
+    public function expandAll (array $urls, callable $callback = null)
+    {
+        $urls = array_values($urls);
+        $expanded = [];
 
-        if (! isset($json->short_url)) {
-            throw new URLException("bad response: expected property `short_url`");
-        } else {
-            return $json->short_url;
+        // Request builder.
+        $requests = function ($urls) {
+            foreach ($urls as $url) {
+                yield new Request('HEAD', $url);
+            }
+        };
+
+        if (! is_callable($callback)) {
+            $expanded = array_combine($urls, array_pad([], count($urls), null));
+            $callback = function ($error, $long, $short) use (&$expanded) {
+                if (! $error) {
+                    $expanded[$short] = $long;
+                }
+            };
         }
+
+        $client = clone $this->client;
+        $client->setAccessToken(null);
+
+        (new Pool(
+            $client,
+            $requests($urls),
+            [
+                'fulfilled' => function (ResponseInterface $response, $index) use ($urls, $callback) {
+                    $short = $urls[$index];
+                    $long = $response->hasHeader('Location') ? $response->getHeaderLine('Location') : null;
+
+                    call_user_func($callback, null, $long, $short);
+                },
+                'rejected'  => function (RequestException $reason) use ($callback) {
+                    call_user_func($callback, $reason, null, null);
+                },
+            ]
+        ))->promise()->wait();
+
+        return $expanded;
     }
 
     /**
@@ -49,26 +167,12 @@ class URLService extends Service
      */
     public function expand ($url)
     {
-        $client = clone $this->client;
-        $client->setAccessToken(null);
-
-        $destination = null;
-        $request = new Request("HEAD", $url);
-
-        $client->send($request, [
-            RequestOptions::ALLOW_REDIRECTS => [
-                'max'         => 10,
-                'strict'      => true,
-                'on_redirect' => function (
-                    RequestInterface $request,
-                    ResponseInterface $response,
-                    UriInterface $uri
-                ) use (&$destination) {
-                    $destination = (string)$uri;
-                },
-            ],
-        ]);
-
-        return $destination;
+        $expanded = null;
+        
+        $this->expandAll([$url], function ($error, $long) use (&$expanded) {
+            $expanded = $long;
+        });
+        
+        return $expanded;
     }
 }
