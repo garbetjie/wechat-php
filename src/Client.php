cf66a74b7283d2a8059d880b036d684a15c3cdeb
Geoff Garbers <geoff@garbers.co.za>
2016-07-04 07:24:25 +0200

    Added immutability of client when adding access token.

--- src/Client.php
+++ src/Client.php
@@ -14,7 +14,7 @@ use Garbetjie\WeChatClient\Authentication\AccessToken;
 class Client extends GuzzleClient
 {
     /**
-     * @var AccessToken
+     * @var AccessToken|null
      */
     protected $token;
 
@@ -32,145 +32,154 @@ class Client extends GuzzleClient
         if (isset($config['developerMode'])) {
             $this->developerMode = !! $config['developerMode'];
             unset($config['developerMode']);
-        }   
+        }
 
         if (! isset($config['handler'])) {
             $config['handler'] = HandlerStack::create();
         }
 
-        // Set the response filter.
-        $config['handler']->remove('wechatClient:response');
-        $config['handler']->push($this->createResponseHandler(), 'wechatClient:response');
+        if (isset($config['wechat.accessToken'])) {
+            if ($config['wechat.accessToken'] instanceof AccessToken) {
+                $this->token = $config['wechat.accessToken'];
+            } else {
+                throw new \InvalidArgumentException("`wechat.accessToken` config item must be an instance of `" . AccessToken::class . "`");
+            }
+        }
 
-        // Set the authentication.
-        $config['handler']->remove('wechatClient:authentication');
-        $config['handler']->unshift($this->createAuthenticationHandler(), 'wechatClient:authentication');
+        if (isset($config['wechat.developerMode'])) {
+            $this->developerMode = !! $config['wechat.developerMode'];
+        }
 
-        // Set the WeChat developer middleware.
-//        $config['handler']->remove('wechatClient:developerMode');
-//        $config['handler']->unshift($this->createDeveloperModeHandler(), 'wechatClient:developerMode');
+        $this->configureHandlers($config['handler']);
 
         // Call the parent's constructor.
         parent::__construct($config);
     }
-
-    /**
-     * Creates the middleware required for validating API responses.
-     *
-     * @return \Closure
-     */
-    private function createResponseHandler ()
+    
+    private function configureHandlers (HandlerStack $handlerStack)
     {
-        return function (callable $handler) {
-            return function (RequestInterface $request, array $options = []) use ($handler) {
-                return $handler($request, $options)->then(
-                    function (ResponseInterface $response) use ($request) {
-                        // Non-success page, so we won't attempt to parse.
-                        if ($response->getStatusCode() >= 300) {
+        $this->configureResponseHandler($handlerStack);
+        $this->configureAuthenticationHandler($handlerStack);
+    }
+    
+    private function configureResponseHandler (HandlerStack $handlerStack)
+    {
+        $handlerStack->remove('wechatClient:response');
+        $handlerStack->push(
+            'wechatClient:response',
+            function (callable $handler) {
+                return function (RequestInterface $request, array $options = []) use ($handler) {
+                    return $handler($request, $options)->then(
+                        function (ResponseInterface $response) use ($request) {
+                            // Non-success page, so we won't attempt to parse.
+                            if ($response->getStatusCode() >= 300) {
+                                return $response;
+                            }
+
+                            // Check if the response should be JSON decoded
+                            $parse = ['application/json', 'text/json', 'text/plain'];
+                            if (preg_match('#' . implode('|', $parse) . '#', $response->getHeaderLine('Content-Type')) < 1) {
+                                return $response;
+                            }
+
+                            // Begin parsing JSON body.
+                            $body = (string)$response->getBody();
+                            $json = json_decode($body);
+
+                            if (json_last_error() !== JSON_ERROR_NONE) {
+                                throw new BadResponseFormatException(json_last_error_msg(), json_last_error());
+                            }
+
+                            if (isset($json->errcode) && $json->errcode != 0) {
+                                $message = isset($json->errmsg) ? $json->errmsg : '';
+                                $code = $json->errcode;
+
+                                throw new APIErrorException($message, $code, $request, $response);
+                            }
+
                             return $response;
                         }
-
-                        // Check if the response should be JSON decoded
-                        $parse = ['application/json', 'text/json', 'text/plain'];
-                        if (preg_match('#' . implode('|', $parse) . '#', $response->getHeaderLine('Content-Type')) < 1) {
-                            return $response;
-                        }
-
-                        // Begin parsing JSON body.
-                        $body = (string)$response->getBody();
-                        $json = json_decode($body);
-
-                        if (json_last_error() !== JSON_ERROR_NONE) {
-                            throw new BadResponseFormatException(json_last_error_msg(), json_last_error());
-                        }
-
-                        if (isset($json->errcode) && $json->errcode != 0) {
-                            $message = isset($json->errmsg) ? $json->errmsg : '';
-                            $code = $json->errcode;
-                            
-                            throw new APIErrorException($message, $code, $request, $response);
-                        }
-
-                        return $response;
-                    }
-                );
-            };
-        };
+                    );
+                };
+            }
+        );
     }
-
-    /**
-     * Returns a callback function that is used as middleware in the Guzzle request.
-     *
-     * @return \Closure
-     */
-    private function createAuthenticationHandler ()
+    
+    private function configureAuthenticationHandler (HandlerStack $handlerStack)
     {
-        $token = &$this->token;
-
-        return function (callable $handler) use (&$token) {
-            return function (RequestInterface $request, array $options = []) use ($handler, &$token) {
-                if ($token) {
-                    $newUri = Uri::withQueryValue($request->getUri(), 'access_token', (string)$token);
-                    $request = $request->withUri($newUri);
-                }
-
-                return $handler($request, $options);
-            };
-        };
-    }
-
-    /**
-     * Returns a callback function that is used to alter the destination host, depending on whether developer mode is
-     * being used or not.
-     *
-     * @return \Closure
-     */
-    private function createDeveloperModeHandler ()
-    {
-        $developerMode = &$this->developerMode;
-
-        return function (callable $handler) use (&$developerMode) {
-            return function (RequestInterface $request, array $options = []) use ($handler, &$developerMode) {
-                if ($developerMode) {
-                    $mapping = [
-                        'api.weixin.qq.com' => 'api.devcentral.co.za',
-                    ];
-
-                    $uri = $request->getUri();
-
-                    if (isset($mapping[$uri->getHost()])) {
-                        $uri = $uri->withHost($mapping[$uri->getHost()]);
+        $token = $this->token;
+        
+        $handlerStack->remove('wechatClient:authentication');
+        $handlerStack->unshift(
+            'wechatClient:authentication',
+            function (callable $handler) use ($token) {
+                return function (RequestInterface $request, array $options = []) use ($handler, $token) {
+                    if ($token) {
+                        $newUri = Uri::withQueryValue($request->getUri(), 'access_token', $token->value());
+                        $request = $request->withUri($newUri);
                     }
 
-                    $request = $request->withUri($uri);
-                }
-
-                return $handler ($request, $options);
-            };
-        };
+                    return $handler($request, $options);
+                };
+            }
+        );
     }
-
-    /**
-     * Sets the authentication token to be used in subsequent requests.
-     *
-     * Setting the token value to NULL will clear the token being used.
-     *
-     * @param AccessToken $token
-     */
-    public function setAccessToken (AccessToken $token = null)
+    
+    private function configureDeveloperModeHandler (HandlerStack $handlerStack)
     {
-        $this->token = $token;
+        $developerModeEnabled = $this->developerMode;
+        
+        $handlerStack->remove('wechatClient:developerMode');
+        $handlerStack->unshift(
+            'wechatClient:developerMode',
+            function (callable $handler) use ($developerModeEnabled) {
+                return function (RequestInterface $request, array $options = []) use ($handler, $developerModeEnabled) {
+                    if ($developerModeEnabled) {
+                        $mapping = [
+                            'api.weixin.qq.com' => 'api.devcentral.co.za',
+                            'api.wechat.com'    => 'api.devcentral.co.za',
+                        ];
+
+                        $uri = $request->getUri();
+
+                        if (isset($mapping[$uri->getHost()])) {
+                            $uri = $uri->withHost($mapping[$uri->getHost()]);
+                        }
+
+                        $request = $request->withUri($uri);
+                    }
+
+                    return $handler ($request, $options);
+                };
+            }
+        );
     }
 
     /**
-     * Indicate whether or not we're using the WeChat developer portal as a proxy.
+     * Sets the access token to be used, and returns the cloned client with the token being used.
      * 
-     * *** PLEASE NOTE: THIS HAS NOT YET BEEN IMPLEMENTED. ***
+     * @param AccessToken $token
      *
-     * @param bool $developerMode
+     * @return Client
      */
-//    public function setDeveloperMode ($developerMode)
-//    {
-//        $this->developerMode = !! $developerMode;
-//    }
+    public function withAccessToken (AccessToken $token)
+    {
+        $config = $this->getConfig();
+        $this->configureHandlers($config['handler']);
+        
+        $config['wechat.accessToken'] = $token;
+        $config['wechat.developerMode'] = $this->developerMode;
+        
+        return new static($config);
+    }
+
+    /**
+     * Returns the access token currently being used by the client.
+     * 
+     * @return AccessToken|null
+     */
+    public function getAccessToken ()
+    {
+        return $this->token;
+    }
 }
